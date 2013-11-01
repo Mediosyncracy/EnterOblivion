95b491063b76ea8741f11ccbfeba12ae8786009f
Raymond Finzel <prometheoid@gmail.com>
2013-11-01 16:37:39 -0500

    things boilerplate

---
+++ things.js
@@ -0,0 +1,44 @@
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+var renderer = new THREE.WebGLRenderer();
+
+var scale = 20;
+
+renderer.setSize( window.innerWidth, window.height );
+document.body.appendChild( renderer.domElement );
+
+init();
+anim();
+
+
+function init(){
+
+    
+
+
+
+
+
+
+
+
+
+}
+
+
+
+function anim(){
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
