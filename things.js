3f6de27acfd18387833b86c6d17dfe24cf96fb4e
Raymond Finzel <prometheoid@gmail.com>
2013-11-01 17:20:28 -0500

    test shell things.js

--- things.js
+++ things.js
@@ -2,7 +2,14 @@ var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 var renderer = new THREE.WebGLRenderer();
 
-var scale = 20;
+var fieldsize = 20;
+
+var geometry = new THREE.CubeGeometry(.1,.1,.1);
+var voxel = new THREE.Mesh(geometry);
+var amalgam = new THREE.Geometry();
+var material = new THREE.MeshPhongMaterial({color: 0x282828 });
+
+
 
 renderer.setSize( window.innerWidth, window.height );
 document.body.appendChild( renderer.domElement );
@@ -12,16 +19,20 @@ anim();
 
 
 function init(){
-
-    
-
-
-
-
-
+    var position = THREE.Vector3();
+    for(var x=0,x<fieldsize,x++){
+	for(var y=0,y<fieldsize,y++){
+	    for(var z=0,z<fieldsize,z++){
+		position.set(x,y,z);
 
 
+		// Procedural Fractal Generation Here
 
+		voxel.setPosition(position);
+		THREE.GeometryUtils.merge(amalgam, voxel);
+	    }
+	}
+    }
 
 }
 
@@ -29,16 +40,8 @@ function init(){
 
 function anim(){
 
-
-
-
-
-
-
-
-
-
-
+    requestAnimationFrame(anim);
+    renderer.render(scene, camera);
 
 
 }
