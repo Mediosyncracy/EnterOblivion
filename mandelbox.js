72686f699babd6cfa586516038cee75d518cdac5
Sythe2o0 <Sythe2o0@gmail.com>
2013-11-02 01:26:32 -0500

    Update mandelbox.js

--- mandelbox.js
+++ mandelbox.js
@@ -7,7 +7,7 @@ function mandelbox (v){
 	var fixed_radius = 1;
 	var min_radius = 0.5;
 	var mag_sum = 0;
-	var constant = 0;
+	var constant = 4;
 
 	for (var i = 0; i < 3; i++){
 		var element = v.getComponent(i);
@@ -18,22 +18,30 @@ function mandelbox (v){
 			v.setComponent(i, (-folding_val - element));
 		}
 		// Prepare magnitude
+		//console.log("Twenty One : " + v.x);
 		mag_sum += Math.pow(element,2);
 	}
 
 	// Generate magnitude
 	magnitude = Math.pow(mag_sum,0.5);
 
+	//console.log("Twenty Seven : " + v.x);
 	if (magnitude < min_radius){
+		//console.log("FIRST IF");
 		v.multiplyScalar(Math.pow(fixed_radius,2)/Math.pow(min_radius,2));
+		//console.log(v.x);
 	}
 	else if (magnitude < fixed_radius){
+		//console.log("Second IF");
 		var x = Math.pow(magnitude,2);
+		//console.log(v.x);
 		v.multiplyScalar(Math.pow(fixed_radius,2)/Math.pow(magnitude,2));
 	}
-
-	v.multiplyScalar(scale * v);
-	v += constant;
-	console.log("mandelbox worked");
+	
+	//console.log("Forty One : " + v.x);
+	v.multiplyScalar(scale);
+	v.addScalar(constant);
+	//console.log("Forty Four : " + v.x);
+	//console.log("mandelbox worked");
 	return v;
 }	
